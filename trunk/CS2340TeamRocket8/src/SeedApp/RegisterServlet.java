7417223ae3580835ec0d608b3aa667a8b6997a92
eugenepark3@gmail.com <eugenepark3@gmail.com@b5ee65d0-b470-4bbd-7c95-232d7da0eec8>
2012-07-06 06:52:56 +0000

    with javadoc

--- trunk/CS2340TeamRocket8/src/SeedApp/RegisterServlet.java
+++ trunk/CS2340TeamRocket8/src/SeedApp/RegisterServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
+ * @author Team Rocket
+ * @version 1.0
  * Servlet implementation class RegisterServlet
  */
 @WebServlet("/RegisterServlet")
